432982624abf55e2fccf124c9b2f0cdfd37afbaf
Frank Piesik <f.piesik@gmail.com>
2026-08-31 18:39:26 +0200

    Revert "Merge pull request #2 from fpiesik/codex/implementiere-gamepadmanager-mit-controller-inspector"
    This reverts commit 36afb8c7daa856ba9b4e9246a869b5af6a383b4a, reversing
    changes made to 96bbdae2d685e86b76252e8747c145bdc9e029fd.

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,19 @@
 import "./style.css";
 import { FixedTimestep } from "./core/FixedTimestep";
-import { ControllerInspector } from "./input/ControllerInspector";
-import { GamepadManager } from "./input/GamepadManager";
 import { Scene } from "./render/Scene";
 import { createPhysicsWorld } from "./simulation/PhysicsWorld";
 
 async function start(): Promise<void> {
   const canvas = document.querySelector<HTMLCanvasElement>("#simulator");
   if (!canvas) throw new Error("Simulator canvas is missing");
-  const inspectorElement = document.querySelector<HTMLElement>("#controllers");
-  if (!inspectorElement) throw new Error("Controller inspector is missing");
 
   const view = new Scene(canvas);
-  const gamepadManager = new GamepadManager();
-  const controllerInspector = new ControllerInspector(inspectorElement);
-  gamepadManager.subscribe((gamepads) => controllerInspector.render(gamepads));
   const { world, cube } = await createPhysicsWorld();
   const physicsLoop = new FixedTimestep(120);
   let previousTime = performance.now();
   document.querySelector<HTMLElement>("#loading")?.setAttribute("hidden", "");
 
   const frame = (time: number): void => {
-    gamepadManager.update();
     physicsLoop.advance((time - previousTime) / 1000, (stepSeconds) => {
       world.timestep = stepSeconds;
       world.step();
